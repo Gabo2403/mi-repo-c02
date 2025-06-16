e224511e8e6e64ba1b19fa356a6f22f313136938
Gabriel <gabo24jgl@gmail.com>
2025-06-15 23:22:32 -0600

    3er commit y final de entrega
    Commit final de la tarea 1

--- src/main/java/com/project/demo/logic/entity/product/ProductRepository.java
+++ src/main/java/com/project/demo/logic/entity/product/ProductRepository.java
@@ -7,22 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    List<Product> findByCategory(Category category);
-
-    List<Product> findByNameContainingIgnoreCase(String name);
-
-    List<Product> findByPriceBetween(Double minPrice, Double maxPrice);
-
-    List<Product> findByStockGreaterThan(Integer stock);
-
-    List<Product> findByStockEquals(Integer stock);
-
-    void deleteByCategory(Category category);
-
-    long countByCategory(Category category);
-
-    List<Product> findByCategoryAndStockGreaterThan(Category category, Integer stock);
-
 }
 
